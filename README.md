1aff6b1b709e2ce64829c898db1954fb5222b8c3
Joseph Wong-Ludwiczak <josclud@gmail.com>
2020-11-19 19:22:38 -0800

    temp lits

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# joseph
+# hello kitty 
+ 
+  Description about the app: here is an app about an app 
+ Information on this app: google it somemore  
+ 
+  Contributors: me and my self 
 
+  Instructions: google it again  
+ 
+  Github user name: joclud 
+ 
+  Email: joclud@gmail.com
--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,8 +1,13 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
-
-`;
+  return `# ${data.title} \n 
+  Description about the app: ${data.description} \n 
+  
+  Information on this app: ${data.usageInformation} \n 
+  Contributors: ${data.contribution} \n
+  Instructions: ${data.testInstructions} \n 
+  Github user name: ${data.github} \n 
+  Email: ${data.email}`
 }
 
 module.exports = generateMarkdown;
